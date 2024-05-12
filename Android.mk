26599385f70d76841c2e8665a6c0a4bde3da120d
SirRGB <sirrgb@proton.me>
2024-05-12 12:45:02 +0200

    update Fennec prebuilt
    * https://f-droid.org/de/packages/org.mozilla.fennec_fdroid
    
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := Fennec
-LOCAL_SRC_FILES := org.mozilla.fennec_fdroid_1242020.apk
+LOCAL_SRC_FILES := org.mozilla.fennec_fdroid_1253020.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
