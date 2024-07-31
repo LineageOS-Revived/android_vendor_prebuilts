2db57802337563158b2f9df041e76fc4606da604
SirRGB <sirrgb@proton.me>
2024-07-31 19:23:08 +0200

    update Fennec prebuilts to v128.0.1 + add arm build
    * https://f-droid.org/en/packages/org.mozilla.fennec_fdroid

--- Android.mk
+++ Android.mk
@@ -1,9 +1,13 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := Fennec
-LOCAL_SRC_FILES := org.mozilla.fennec_fdroid_1270220.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
+
+LOCAL_MODULE_TARGET_ARCH := arm arm64
+my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
+LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/fennec.apk
 include $(BUILD_PREBUILT)
