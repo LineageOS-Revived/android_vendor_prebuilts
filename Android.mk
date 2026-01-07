f365c30b18c385c3d2ef685e63c99fec37f28809
SirRGB <sirrgb@proton.me>
2026-01-07 21:03:28 +0100

    feat!: deprecate fennec in favour of fulguris
    * fennec now requires Android 8+

--- Android.mk
+++ Android.mk
@@ -2,12 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := Fennec
+LOCAL_MODULE := Fulguris
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
 
 LOCAL_MODULE_TARGET_ARCH := arm arm64
-my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
-LOCAL_SRC_FILES := $(my_src_arch)/fennec.apk
+LOCAL_SRC_FILES := fulguris.apk
 include $(BUILD_PREBUILT)
