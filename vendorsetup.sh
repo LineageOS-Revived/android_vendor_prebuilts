4febc9aedf1cbae6abfd118bda9f03f5f3cfafd3
SirRGB <sirrgb@proton.me>
2026-01-19 22:58:24 +0100

    chore: log dl url

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,6 +3,8 @@
 set -euo pipefail
 
 version=$(curl --fail --silent --show-error --request GET https://f-droid.org/api/v1/packages/net.slions.fulguris.full.fdroid | python3 -c 'import json,sys; print(json.load(sys.stdin)["suggestedVersionCode"])')
-curl https://f-droid.org/repo/net.slions.fulguris.full.fdroid_"${version}".apk --output "$(dirname ${BASH_SOURCE[0]})"/fulguris.apk
+dl_url=https://f-droid.org/repo/net.slions.fulguris.full.fdroid_"${version}".apk
+echo "${dl_url}"
+curl --fail --silent --show-error "${dl_url}" --output "$(dirname ${BASH_SOURCE[0]})"/fulguris.apk
 
 set +euo pipefail
