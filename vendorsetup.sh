acefdf59f45b1bb083cfb0a7da1fdc503bf9d52d
SirRGB <sirrgb@proton.me>
2026-06-04 17:43:32 +0200

    fix: move to return

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 find "$(dirname "${BASH_SOURCE[0]}")"/ -name "*.apk" -ctime +1 -exec rm {} \;
-[[ -f "$(dirname "${BASH_SOURCE[0]}")"/fulguris.apk ]] && exit
+[[ -f "$(dirname "${BASH_SOURCE[0]}")"/fulguris.apk ]] && return
 version=$(curl --fail --silent --show-error --request GET https://f-droid.org/api/v1/packages/net.slions.fulguris.full.fdroid | python3 -c 'import json,sys; print(json.load(sys.stdin)["suggestedVersionCode"])')
 dl_url=https://f-droid.org/repo/net.slions.fulguris.full.fdroid_"${version}".apk
 echo "${dl_url}"
